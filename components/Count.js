3f631bcd912cdf240e28753b49bdb306f62340c1
L-David-26 <davidlez2mgk@gmail.com>
2021-09-22 21:30:16 -0500

    full complete cdn of react

---
+++ components/Count.js
@@ -0,0 +1,3 @@
+const Contador = () => {
+        return (<h2> c o n t a d o r  :  0</h2>)
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const $root = document.querySelector(".root");
 const $RD = ReactDOM;
 $RD.render(
-      <Contador />,
+      <Count />,
   $root
 );
